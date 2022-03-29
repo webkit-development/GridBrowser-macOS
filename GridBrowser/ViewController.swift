c352e19b57b4ac400d5b0bdf39eba9496af117a4
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-03-29 19:08:57 -0400

    Initial Commit

---
+++ GridBrowser/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GridBrowser
+//
+//  Created by Kevin Stradtman on 3/29/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
